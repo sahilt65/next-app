7ba26042ea4cc79a4acfc6dd542b3073e574d259
sahilt65 <sahiltpict@gmail.com>
2025-02-25 16:10:40 +0530

    add chakra-tabs

--- src/app/chakra-impl/components/page.tsx
+++ src/app/chakra-impl/components/page.tsx
@@ -1,10 +1,43 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { fonts } from "@/app/constants/fonts";
+import {
+  Box,
+  Heading,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+import React from "react";
 
 const Components = () => {
   return (
-    <Box bg="GrayText">This is a box</Box>
-  )
-}
+    <>
+      <Heading size='16' >Tabs</Heading>
+      <Tabs isManual isFitted variant="soft-rounded" colorScheme="green" >
+        <TabList>
+          <Tab>One</Tab>
+          <Tab>Two</Tab>
+          <Tab>Three</Tab>
+        </TabList>
 
-export default Components
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>four!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+
+
+    
+    </>
+  );
+};
+
+export default Components;
