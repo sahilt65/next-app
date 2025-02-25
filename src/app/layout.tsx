fdb9b528397d66e99de756b9f93435f318382fb1
sahilt65 <sahiltpict@gmail.com>
2025-02-25 15:28:38 +0530

    chakra actual setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Providers } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ChakraProvider>
+        <Providers>
         {children}
-        </ChakraProvider>
+        </Providers>
       </body>
     </html>
   );
