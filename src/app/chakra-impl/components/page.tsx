723025e87280d040f24c6f85dde93cb29c5855dc
sahilt65 <sahiltpict@gmail.com>
2025-02-25 15:20:14 +0530

    chakra ui setup

---
+++ src/app/chakra-impl/components/page.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const Components = () => {
+  return (
+    <Box bg="GrayText">This is a box</Box>
+  )
+}
+
+export default Components
