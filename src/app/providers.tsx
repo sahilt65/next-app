757436b3b886a8fa52e49361bd054a2e64106b78
sahilt65 <sahiltpict@gmail.com>
2025-02-26 20:20:47 +0530

    trying react query

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,14 @@
 import { SaasProvider } from "@saas-ui/react";
 import React from "react";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 
+const queryClient = new QueryClient({});
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <SaasProvider>{children}</SaasProvider>;
+  return <QueryClientProvider client={queryClient}>
+    <SaasProvider>
+      {children}
+    </SaasProvider>
+  </QueryClientProvider>;
 };
 
 export default Providers;
---
+++ src/app/react-query/page.tsx
@@ -0,0 +1,41 @@
+'use client';
+import { useQuery } from "@tanstack/react-query";
+
+type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+const ReactQuery = () => {
+  const { data, isLoading, error } = useQuery<Todo[]>({
+    queryKey: ["key-1"],
+    queryFn: async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const json = await response.json();
+      return JSON.parse(JSON.stringify(json));
+    },
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching data</p>;
+
+  return (
+    <div>
+      {data?.map((todo) => (
+        <div key={todo.id}>
+          <h1>Id: {todo.id}</h1>
+          <h1>Title: {todo.title}</h1>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ReactQuery;
