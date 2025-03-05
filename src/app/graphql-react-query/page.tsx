ee26bf11adac4a28242ff7decc7cd93c16b93cad
sahilt65 <sahiltpict@gmail.com>
2025-03-05 14:58:18 +0530

    formatted code

--- src/app/graphql-react-query/page.tsx
+++ src/app/graphql-react-query/page.tsx
@@ -1,19 +1,24 @@
-'use client'
-import { GetUserLimitedDetailsQuery, useGetUserLimitedDetailsQuery, useUserQuery } from "@/generated";
+"use client";
+import {
+  GetUserLimitedDetailsQuery,
+  useGetUserLimitedDetailsQuery,
+  useUserQuery,
+} from "@/generated";
 import React from "react";
 
 const GraphQLReactQuery = () => {
-        const { data, error, isLoading } = useGetUserLimitedDetailsQuery<GetUserLimitedDetailsQuery>({ id: 1 });
+  const { data, isLoading } =
+    useGetUserLimitedDetailsQuery<GetUserLimitedDetailsQuery>({ id: 1 });
 
-        if (isLoading) return <p>Loading...</p>;
-    
-        return (
-        <div>
-            <h2>{data?.userById!.name}</h2>
-            <p>Phone: {data?.userById!.phone}</p>
-            <p>Website: {data?.userById!.website}</p>
-        </div>
-        );
+  if (isLoading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h2>{data?.userById!.name}</h2>
+      <p>Phone: {data?.userById!.phone}</p>
+      <p>Website: {data?.userById!.website}</p>
+    </div>
+  );
 };
 
 export default GraphQLReactQuery;
